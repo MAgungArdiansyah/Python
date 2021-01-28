bbdc8fdc10460b1997a897bfeca4a0fa36f49f70
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-01-28 18:18:44 +0700

    Update SAO_CommadLine.py

--- ProgramSederhana/SAO_CommadLine.py
+++ ProgramSederhana/SAO_CommadLine.py
@@ -1,7 +1,21 @@
-class hero:
+class Hero:
     def __init__(self, name, hp, attcpow, armor):
         self.Name = name
         self.Hp = hp
         self.AttPow = attcpow
         self.Armor = armor
+    
+    def serang(self,lawan):
+        lawan.Hp = lawan.Hp - (self.AttPow - (self.Armor*0.5))
+        print(self.Name,'menyerang',lawan.Name)
+        print(lawan.Name,'HP tersisa: ', lawan.Hp)
+        return 'ahn..!!'
+    
+    def diserahg
+    
+
+kirito = Hero('Kirito', 300, 100, 50)
+asuna = Hero('Asuna', 250, 150, 25)
+
+print(kirito.serang(asuna))
     
