36bebfd0a4255229541183b20aaed1f7a36f03cd
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-01-30 10:17:45 +0700

    belajar lagi

--- PythonOOP/6_Encapsulasi.py
+++ PythonOOP/6_Encapsulasi.py
@@ -13,6 +13,16 @@ class Hero:
     # getter
     def getName(self):
         return self.__Name
+    
+    def getHealth(self):
+        return self.__Hp
+
+    # Setter
+    def setAtt(self, NewAtt):
+        self.__Att = NewAtt
+    
+    def diserang(self, AttPow):
+        self.__Hp -= AttPow
 
 
 # awal game sao
@@ -20,4 +30,8 @@ kirito = Hero('Kirito', 300, 100)
 
 # game berjalan
 print(kirito.getName())
+print(kirito.getHealth())
+kirito.diserang(100)
+print(kirito.getHealth())
+
         
