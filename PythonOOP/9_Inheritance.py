2ef318aa7eb89f82282fd7961a0ea7b64810f08c
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-02-02 10:52:56 +0700

    belajar lagi

---
+++ PythonOOP/9_Inheritance.py
@@ -0,0 +1,11 @@
+class BangunDatar:
+
+    def __init__(self, sisi):
+        self._sisi = sisi
+
+class PersegiPanjang(Persegi):
+    def __init__(self, sisi):
+        self.panjang = super().__init__(sisi)
+
+
+    
